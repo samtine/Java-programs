b0c3df6a48e0b37581dcedbf6bc64a8043a859b5
samtine <samtinesp@gmail.com>
2017-04-02 14:18:07 +0530

    primeno

--- PrimeNoGeneration.java
+++ PrimeNoGeneration.java
@@ -3,18 +3,19 @@ public class PrimeNoGeneration {
 public int a,b,i,j;
 public void diplay(){
 	b=0;
-	System.out.println("2");		
-	for(i=3;b<10;i++){
+	System.out.println("2");
+	System.out.println("3");
+	for(i=3;b<8;i++){
 		for(j=2;j<i/2;j++){
 			if(i%j==0){
-				a=1;
+				a=0;
 				break;
 			}
 			else{
-				a=0;
+				a=1;
 			}
 		}
-		if(a==0){
+		if(a==1){
 			b=b+1;
 			System.out.println(i);
 		}
